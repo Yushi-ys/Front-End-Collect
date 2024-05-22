3664a734f56938fa1a5d2582e640fa59ac925180
施涛 <19551555337@163.com>
2024-05-22 16:52:50 +0800

    feat: 提交

---
+++ HTML/html5的新特性.md
@@ -0,0 +1,6 @@
+1. 语义元素，例如<header><footer><nav><section>等，更好的 seo，提高代码可读性
+2. 多媒体支持，例如<audio><video>，使音频嵌入更加简单
+3. Canvas，允许通过 js 取创建和操作图形
+4. WebSocket，一种实时通信的协议，可用于创建实时聊天和多人游戏
+5. 拖放 api
+6. 新的事件 api，addEventListener
--- 测试.js
+++ 测试.js
@@ -1,1 +1,31 @@
-"我是测试分支的"
+const url =
+  "https://mars-dev-persistent.oss-cn-hangzhou.aliyuncs.com/mars-file/660410797238878209/91f9af2b-bf34-48c8-84f5-1648430b7ca6/1707118208382apple1.jpeg";
+
+const image2Base64 = (url) =>
+  new Promise((resolve, reject) => {
+    if (!url) {
+      resolve("");
+      return;
+    }
+    const img = new Image();
+    img.crossOrigin = "anonymous";
+
+    img.src = url;
+    img.onload = () => {
+      const canvas = document.createElement("canvas");
+      canvas.width = img.width;
+      canvas.height = img.height;
+      const ctx = canvas.getContext("2d");
+      ctx?.drawImage(img, 0, 0);
+      const data = canvas.toDataURL();
+
+      resolve(data);
+    };
+    img.onerror = () => {
+      reject("");
+    };
+  });
+
+image2Base64(url).then((res) => {
+  console.log(res);
+});
